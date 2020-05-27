ecb2f89c616c5e65f0bf10997e9900043f1fbc2c
Ruben Cancino <rubencancino6@gmail.com>
2020-05-27 09:42:22 -0500

    added cartIcon and cart-dropdown components to our application, also implemented cart reducer

--- src/App.js
+++ src/App.js
@@ -48,6 +48,7 @@ class App extends React.Component {
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
           <Route
+            exact
             path="/signin"
             render={() =>
               this.props.currentUser ? (
