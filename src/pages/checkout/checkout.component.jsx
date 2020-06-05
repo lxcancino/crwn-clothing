840d3bdb2e96ebdf745cccf85e340ecd1abf9c4a
Ruben Cancino <rubencancino6@gmail.com>
2020-06-05 08:26:29 -0500

    Last

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -9,6 +9,7 @@ import {
 import './checkout.styles.scss';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 const CheckoutPage = ({ cartItems, total }) => {
   return (
@@ -36,6 +37,12 @@ const CheckoutPage = ({ cartItems, total }) => {
       <div className="total">
         <span>TOTAL: {total}</span>
       </div>
+      <div className="test-warning">
+        *Pleas use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: Any future date - CVV: 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 };
